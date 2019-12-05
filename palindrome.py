b581d3708d61bb581878c84b7dbe8084b133e65e
Brenton <bren10@gmail.com>
2019-12-04 16:15:09 -0800

    added new palindrome program

---
+++ palindrome.py
@@ -0,0 +1,25 @@
+'''
+This program determines if a given sentence is a palindrome or not
+'''
+def isPalindrome(s):
+  
+  def toChars(s):
+     s = s.lower()
+     ans = ''
+     for c in s:
+        if c in "abcdefghijklmnopqrstuvwxyz":
+           ans = ans + c     
+     return ans
+  
+  def isPal(s):
+     if len(s) <= 1:
+        return True
+     else:
+        return s[0] == s[-1] and isPal(s[1:-1])
+  return isPal(toChars(s))
+
+s = input("Give sentence: ")
+if isPalindrome(s):
+  print("That shit WAS a palindrome.")
+else:
+  print("That shit was NOT a palindrome.")
