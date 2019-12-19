bbe2d6f99cd38a6fa7a06830a904429695d0575a
Brenton <bren10@gmail.com>
2019-12-19 12:17:28 -0800

    fixed tab spacing in palindrome

--- palindrome.py
+++ palindrome.py
@@ -3,23 +3,23 @@ This program determines if a sentence is a palindrome or not
 '''
 def isPalindrome(s):
   
-  def toChars(s):
-     s = s.lower()
-     ans = ''
-     for c in s:
-        if c in "abcdefghijklmnopqrstuvwxyz":
-           ans = ans + c     
-     return ans
+    def toChars(s):
+        s = s.lower()
+        ans = ''
+        for c in s:
+            if c in "abcdefghijklmnopqrstuvwxyz":
+                ans = ans + c     
+        return ans
   
-  def isPal(s):
-     if len(s) <= 1:
-        return True
-     else:
-        return s[0] == s[-1] and isPal(s[1:-1])
-  return isPal(toChars(s))
+    def isPal(s):
+        if len(s) <= 1:
+            return True
+        else:
+         return s[0] == s[-1] and isPal(s[1:-1])
+    return isPal(toChars(s))
 
 s = input("Give sentence: ")
 if isPalindrome(s):
-  print("That WAS a palindrome.")
+    print("That WAS a palindrome.")
 else:
-  print("That was NOT a palindrome.")
+    print("That was NOT a palindrome.")
