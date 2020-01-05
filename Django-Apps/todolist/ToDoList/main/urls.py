06583ab76fc15d502559160b20bf6af51bcce147
Brenton <bren10@gmail.com>
2020-01-04 17:00:03 -0800

    added ToDoList to app, can now see lists under list owner's name

--- Django-Apps/todolist/ToDoList/main/urls.py
+++ Django-Apps/todolist/ToDoList/main/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('page2', views.page2, name='page2'),
     path('<int:id>', views.num, name='num'),
-    path('<str:name>', views.showlist, name='showlist'),
+    path('<str:owner>', views.showlist, name='showlist'),
 ]
--- Django-Apps/todolist/ToDoList/main/views.py
+++ Django-Apps/todolist/ToDoList/main/views.py
@@ -3,16 +3,23 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 
 # Create your views here.
-def index(request):
-    return HttpResponse("""
-        <h1>Brenton is cool</h1>
-        <h2>Look at my cool H2 tag</h2>
-        <h4>Look at my cool H4 tag</h4>
-    """)
+def home(request):
+    return render(request, 'main/base.html', {'name':'test'})
+
+
+def page2(request):
+    return render(request, 'main/home.html', {'name':'big hairy balls'})
+
 
 def num(request, id):
-    return HttpResponse(f'<h1>{id}</h1>')
+    return HttpResponse(
+        f'<h1>{id}</h1>'
+        '<h1>Brenton is cool</h1>'
+        '<h2>Look at my cool H2 tag</h2>'
+        '<h4>Look at my cool H4 tag</h4>'
+        )
 
 
-def showlist(request, name):
-    return HttpResponse(f'<h1>{ToDoList.objects.get(name=name)}</h1>')    
+def showlist(request, owner):
+    ls = ToDoList.objects.get(owner=owner)
+    return render(request, 'main/list.html', {'ls':ls})   
