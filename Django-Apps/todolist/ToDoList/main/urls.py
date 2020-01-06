39e74c527a3c501ba36d0e5b6a67448332ab3701
Brenton <bren10@gmail.com>
2020-01-05 20:54:20 -0800

    forms page working

--- Django-Apps/todolist/ToDoList/main/urls.py
+++ Django-Apps/todolist/ToDoList/main/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('page2', views.page2, name='page2'),
-    path('<int:id>', views.num, name='num'),
-    path('<str:owner>', views.showlist, name='showlist'),
+    path('create/', views.create, name='create'),
+    path('page2/', views.page2, name='page2'),
+    path('<int:id>', views.index, name='index'),
+    # path('<str:owner>', views.showlist, name='showlist'),
+    
 ]
--- Django-Apps/todolist/ToDoList/main/views.py
+++ Django-Apps/todolist/ToDoList/main/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import ToDoList, Item
+
+from .forms import CreateNewList
+from .models import Item, ToDoList
+
 
 # Create your views here.
 def home(request):
@@ -11,15 +14,61 @@ def page2(request):
     return render(request, 'main/home.html', {'name':'big hairy balls'})
 
 
-def num(request, id):
-    return HttpResponse(
-        f'<h1>{id}</h1>'
-        '<h1>Brenton is cool</h1>'
-        '<h2>Look at my cool H2 tag</h2>'
-        '<h4>Look at my cool H4 tag</h4>'
-        )
+# def num(request, id):
+#     return HttpResponse(
+#         f'<h1>{id}</h1>'
+#         '<h1>Brenton is cool</h1>'
+#         '<h2>Look at my cool H2 tag</h2>'
+#         '<h4>Look at my cool H4 tag</h4>')
 
 
-def showlist(request, owner):
-    ls = ToDoList.objects.get(owner=owner)
-    return render(request, 'main/list.html', {'ls':ls})   
+# def showlist(request, owner):
+#     ls = ToDoList.objects.get(owner=owner)
+#     if request.method == 'POST':
+#         pass
+#     return render(request, 'main/list.html', {'ls':ls})   
+
+
+def index(request, id):
+    ls = ToDoList.objects.get(id=id)
+
+    # {'save':['save'], 'c1':['clicked']}
+    if request.method == 'POST':
+        print(request.POST)
+        if request.POST.get('save'):
+            for item in ls.item_set.all():
+                if request.POST.get('c' + str(item.id)) == 'clicked':
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+        elif request.POST.get('new_item'):
+            txt = request.POST.get('new')
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid!")
+
+
+    elif request.POST == 'GET':
+            pass
+
+
+
+    return render(request, 'main/list.html', {'ls':ls})
+
+
+def create(request):
+    if request.method == 'POST':
+        form = CreateNewList(request.POST)
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            t = ToDoList(name=n)
+            t.save()
+
+        return HttpResponseRedirect('/%i' %t.id)
+
+    else:
+        form = CreateNewList()
+    return render(request, 'main/create.html', {'form':form})
