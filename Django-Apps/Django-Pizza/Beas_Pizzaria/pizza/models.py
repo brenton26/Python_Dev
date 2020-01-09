f9a83ce3037137b348dd0b604849fec2d95ace81
Brenton <bren10@gmail.com>
2020-01-09 13:04:43 -0800

    forgot to add some changes

--- Django-Apps/Django-Pizza/Beas_Pizzaria/pizza/models.py
+++ Django-Apps/Django-Pizza/Beas_Pizzaria/pizza/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Size(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self, test):
+        return self.title
+
+class Pizza(models.Model):
+    topping1 = 
+
