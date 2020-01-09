d89ba7c949e1d9ddf4c9631d67cf8d146087b7a6
Brenton <bren10@gmail.com>
2020-01-09 12:57:31 -0800

    edited pizza app

---
+++ Django-Apps/Django-Pizza/Beas_Pizzaria/pizza/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .forms import PizzaForm
+
+# Create your views here.
+def home(request):
+    return render(request, 'pizza/home.html')
+
+
+def order(request):
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            size = filled_form.cleaned_data['size']
+            topping1 = filled_form.cleaned_data['topping1']
+            topping2 = filled_form.cleaned_data['topping2']
+            note = f"Thanks for ordering, bitch. Your {size} {topping1} and {topping2} pizza is on it's way"
+            new_form = PizzaForm()
+            return render(request, 'pizza/order.html', {'pizzaform':new_form, 'note':note})
+    else:
+        form = PizzaForm()
+        return render(request, 'pizza/order.html', {'pizzaform':form})
